e72b09c97af0baefa9a8500e967e75c32a0da1d3
etchb <bhongj91@gmail.com>
2022-03-05 02:10:39 +0900

    del time count

--- app/src/main/java/com/bhongj/rc_week4/DartMainActivity.kt
+++ app/src/main/java/com/bhongj/rc_week4/DartMainActivity.kt
@@ -54,7 +54,6 @@ class DartMainActivity : AppCompatActivity() {
         gridLayoutID = mutableListOf<TextView>(binding.mainPostit1,binding.mainPostit2,binding.mainPostit3,binding.mainPostit4,binding.mainPostit5,binding.mainPostit6,binding.mainPostit7,binding.mainPostit8,binding.mainPostit9,binding.mainPostit10,binding.mainPostit11,binding.mainPostit12,binding.mainPostit13,binding.mainPostit14,binding.mainPostit15,binding.mainPostit16,binding.mainPostit17,binding.mainPostit18,binding.mainPostit19,binding.mainPostit20,binding.mainPostit21,binding.mainPostit22,binding.mainPostit23,binding.mainPostit24,binding.mainPostit25,binding.mainPostit26,binding.mainPostit27,binding.mainPostit28,binding.mainPostit29,binding.mainPostit30,binding.mainPostit31,binding.mainPostit32,binding.mainPostit33,binding.mainPostit34,binding.mainPostit35,binding.mainPostit36,binding.mainPostit37,binding.mainPostit38,binding.mainPostit39,binding.mainPostit40,binding.mainPostit41,binding.mainPostit42,binding.mainPostit43,binding.mainPostit44,binding.mainPostit45)
         textViewID = mutableListOf<TextView>(binding.imgBall1,binding.imgBall2,binding.imgBall3,binding.imgBall4,binding.imgBall5,binding.imgBall6,binding.imgBall7)
 
-//        Log.d("TEST gridLayoutID.size", gridLayoutID.size.toString())
 
         binding.targetScore.text = targetScore.toString()
         binding.myScore.text = myScore.toString()
@@ -65,10 +64,6 @@ class DartMainActivity : AppCompatActivity() {
         val width = resources.displayMetrics.widthPixels
         val height = resources.displayMetrics.heightPixels
         val disDen = resources.displayMetrics.density
-//        Log.d("TEST width", width.toString())
-//        Log.d("TEST height", height.toString())
-//        Log.d("TEST resources.displayMetrics.density", resources.displayMetrics.density.toString())
-
         val moveX_s = 0f*width
         val moveX_f = 0.7f*width
 
@@ -84,7 +79,6 @@ class DartMainActivity : AppCompatActivity() {
         }
 
         val randNum = (1..45).toMutableList().shuffled()
-//        Log.d("TEST randNum", randNum.toString())
         randColorNum = mutableListOf()
         while (randColorNum.size < 12) {
             val tmp = (Math.random() * 45).toInt()
@@ -92,7 +86,6 @@ class DartMainActivity : AppCompatActivity() {
                 randColorNum.add(tmp)
             }
         }
-//        Log.d("TEST randColorNum", randColorNum.toString())
 
         for (idx in 0 .. 44) {
             gridLayoutID[idx].text = randNum[idx].toString()
@@ -137,9 +130,9 @@ class DartMainActivity : AppCompatActivity() {
                     binding.startCount.visibility = View.GONE
                 }
                 while (key < 1) {
+                    key--
                     Thread.sleep(100)
                 }
-                key--
                 runLoop = true
                 key++
             }.start()
@@ -163,19 +156,10 @@ class DartMainActivity : AppCompatActivity() {
                     pointY_s = v.y
                     moveX = v.x - event.rawX
                     moveY = v.y - event.rawY
-//                    Log.d("TEST v.x", v.x.toString())
-//                    Log.d("TEST v.y", v.y.toString())
-//                    Log.d("TEST event.rawX", event.rawX.toString())
-//                    Log.d("TEST event.rawY", event.rawY.toString())
                     time_s = System.currentTimeMillis().toInt()
-//                    Log.d("TEST time_s", time_s.toString())
 
                     var location = IntArray(2)
                     binding.imgArrow.getLocationOnScreen(location)
-//                    Log.d("TEST location[0]", location[0].toString())
-//                    Log.d("TEST location[1]", location[1].toString())
-
-//                    binding.rootlay.addView(myView(v.x+v.width/2, v.y, binding.root.context))
 
                 }
 
@@ -191,50 +175,13 @@ class DartMainActivity : AppCompatActivity() {
                     moveX = v.x - event.rawX
                     moveY = v.y - event.rawY
                     pointY_f = v.y
-//                    Log.d("TEST v.x", v.x.toString())
-//                    Log.d("TEST v.y", v.y.toString())
-//                    Log.d("TEST event.rawX", event.rawX.toString())
-//                    Log.d("TEST event.rawY", event.rawY.toString())
-                    shotFlag = true
+
                     time_f = System.currentTimeMillis().toInt()
-//                    Log.d("TEST time_f", time_f.toString())
-//                    Log.d("TEST time_f-time_s", (time_f-time_s).toString())
-//
-//                    Log.d("TEST binding.grdlay.left", binding.grdlay.left.toString())
-//                    Log.d("TEST binding.grdlay.left", binding.grdlay.right.toString())
-//                    Log.d("TEST binding.grdlay.left", binding.grdlay.top.toString())
-//                    Log.d("TEST binding.grdlay.left", binding.grdlay.bottom.toString())
-//                    binding.rootlay.addView(myView(v.x+v.width/2, v.y, binding.root.context))
+                    shotFlag = true
                 }
             }
             true
         }
-
-        Thread() {
-            val handler = Handler(Looper.getMainLooper())
-
-            while(true) {
-                if (gameCnt > 6) {
-                    break
-                }
-//                Log.d("TEST runLoop", runLoop.toString())
-                if (runLoop) {
-                    runLoop = false
-                    handler.post {
-                        anim_arrow.start()
-                        anim_bow.start()
-                    }
-                }
-                Thread.sleep(100)
-            }
-            Thread.sleep(1000)
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("targetScore", targetScore)
-            intent.putExtra("myScore", myScore)
-            intent.putExtra("ballNum", selNum.toIntArray())
-            startActivity(intent)
-            finish()
-        }.start()
 
         Thread() {
             val handler = Handler(Looper.getMainLooper())
@@ -260,10 +207,7 @@ class DartMainActivity : AppCompatActivity() {
                             start()
                         }
                     }
-//                    Log.d("TEST pointY_s", pointY_s.toString())
-//                    Log.d("TEST pointY_f", pointY_f.toString())
-//                    Log.d("TEST max(time_f-time_s,2000)", min(time_f-time_s,2000).toString())
-                    Thread.sleep(1800)
+                    Thread.sleep(1500)
                     handler.post {
                         val pixelOff = 4
                         binding.rootlay.addView(
@@ -280,14 +224,12 @@ class DartMainActivity : AppCompatActivity() {
                         binding.imgArrow.y = pointY_s
 
                         val idx = shotPos[0]*5 + shotPos[1]
-//                        Log.d("TEST idx", idx.toString())
                         if (((0..8).toList().contains(shotPos[0])) && ((0..4).toList().contains(shotPos[1]))) {
                             myScore += getPoint(idx)
                             textViewID[gameCnt].text = gridLayoutID[idx].text.toString()
                             selNum.add(gridLayoutID[idx].text.toString().toInt())
                         }
                         else {
-//                            Log.d("TEST 꽝", "꽝입니다")
                             textViewID[gameCnt].text = "꽝"
                             textViewID[gameCnt].setBackgroundResource(R.drawable.lottery_ball_fail)
                             selNum.add(0)
@@ -319,23 +261,48 @@ class DartMainActivity : AppCompatActivity() {
         }.start()
 
         Thread() {
+            val handler = Handler(Looper.getMainLooper())
+
+            while(true) {
+                if (gameCnt > 6) {
+                    break
+                }
+                if (runLoop) {
+                    runLoop = false
+                    handler.post {
+                        anim_arrow.start()
+                        anim_bow.start()
+                    }
+                }
+                Thread.sleep(100)
+            }
+            Thread.sleep(1000)
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("targetScore", targetScore)
+            intent.putExtra("myScore", myScore)
+            intent.putExtra("ballNum", selNum.toIntArray())
+            startActivity(intent)
+            finish()
+        }.start()
+
+        Thread() {
             var time = 0
             val handler = Handler(Looper.getMainLooper())
             while(binding.startCount.visibility != View.GONE) {
                 Thread.sleep(1000)
             }
-            while(true) {
-                Thread.sleep(1000)
-                if (timeCheck == 1) {
-                    handler.post{
-                        binding.txtTime.text = time.toString()
-                    }
-                    time++
-                    if (time > 999) {
-                        time = 0
-                    }
-                }
-            }
+//            while(true) {
+//                Thread.sleep(1000)
+//                if (timeCheck == 1) {
+//                    handler.post{
+//                        binding.txtTime.text = time.toString()
+//                    }
+//                    time++
+//                    if (time > 999) {
+//                        time = 0
+//                    }
+//                }
+//            }
         }.start()
     }
 
@@ -366,27 +333,16 @@ class DartMainActivity : AppCompatActivity() {
         val width = resources.displayMetrics.widthPixels
         val height = resources.displayMetrics.heightPixels
         val disDen = resources.displayMetrics.density
-//        Log.d("TEST width", width.toString())
-//        Log.d("TEST height", height.toString())
 
         val item_h = height*0.5/9
         val item_w = (width-(20*disDen))/5
-//        Log.d("TEST item_h", item_h.toString())
-//        Log.d("TEST item_w", item_w.toString())
-
-//        Log.d("TEST x", x.toString())
-//        Log.d("TEST y", y.toString())
-
-//        Log.d("TEST height*0.15", (height*0.15).toString())
-//        Log.d("TEST 10*resources.displayMetrics.density", (10*disDen).toString())
 
         var row = ((y-(height*0.15 + 40*disDen))/item_h).toFloat()
         if (row < 0) {
             row = -1f
         }
         var col = ((x-10*disDen)/item_w).toInt()
-//        Log.d("TEST col", col.toString())
-//        Log.d("TEST row", row.toString())
+
         return arrayOf(row.toInt(), col)
     }
 
@@ -398,11 +354,6 @@ class DartMainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
-//        binding.rootlay.addView(myView(100f, 100f, binding.root.context))
-//        binding.rootlay.addView(myView(10*3.5f, 3100*0.15f+40*3.5f, binding.root.context))
-//        binding.rootlay.removeViewAt(binding.rootlay.childCount-1)
-
     }
 
     override fun onStop() {
